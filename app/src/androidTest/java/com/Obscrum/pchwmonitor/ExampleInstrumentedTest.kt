7216128af394af61235f2021bc5114f1a552c03e
obscrum <kaanturan4206@gmail.com>
2026-08-02 23:04:28 +0300

    eski com.example klasörü ve dosyaları temizlendi

--- app/src/androidTest/java/com/Obscrum/pchwmonitor/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/Obscrum/pchwmonitor/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.pchwmonitor", appContext.packageName)
+        assertEquals("com.Obsrum.pchwmonitor", appContext.packageName)
     }
 }
