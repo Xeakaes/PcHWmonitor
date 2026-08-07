7afd3bf856c1dd08cf32176d8e1d2f29a4579757
obscrum <kaanturan4206@gmail.com>
2026-08-07 14:34:32 +0300

    build(app): bump version to 1.4 (versionCode 5)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.Obscrum.pchwmonitor"
         minSdk = 24
         targetSdk = 37
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
